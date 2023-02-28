bb18e13249eff21876265ea0884dc0388652e26c
Manoj M <manumadhu1425@gmail.com>
2023-02-28 23:11:37 +0530

    ArithmeticCalculator class with Add method

---
+++ ArithmeticCalculator/src/com/ArithmeticCalculator/operations/ArithmeticCalculator.java
@@ -0,0 +1,14 @@
+package com.ArithmeticCalculator.operations;
+
+public class ArithmeticCalculator {
+	private int a,b;
+	public ArithmeticCalculator(int a,int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int add() {
+		return a+b;
+	}
+	
+}
