de8dab279ab8dcfb8e481f517db4433d81543c41
Manoj M <manumadhu1425@gmail.com>
2023-02-28 23:18:09 +0530

    Main class with input method

---
+++ ArithmeticCalculator/src/com/ArithmeticCalculator/main/Main.java
@@ -0,0 +1,19 @@
+package com.ArithmeticCalculator.main;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	private static int a , b;
+	private static void input() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter 2 numbers:");
+		a = input.nextInt();
+		b = input.nextInt();
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
