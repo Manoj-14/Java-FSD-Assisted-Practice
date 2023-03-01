5fc13376716b4d1f06771527f6e4857728363764
Manoj M <manumadhu1425@gmail.com>
2023-03-01 20:06:57 +0530

    2.5 Assisted Practice: Constructors

---
+++ JavaFSDAssistedPractices/src/com/assisstedPractices/section2/Constructors25.java
@@ -0,0 +1,19 @@
+package com.assisstedPractices.section2;
+
+public class Constructors25 {
+	public static void main(String[] args) {
+		Constructors c1 = new Constructors();
+		Constructors c2 = new Constructors(2,4);
+		
+	}
+}
+
+class Constructors{
+	Constructors(){
+		System.out.println("Default Constructor");
+	}
+	
+	Constructors(int a,int b){
+		System.out.println("Parameterized constructors with parameters :"+a+","+b);
+	}
+}
