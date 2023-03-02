77351b881765bb1ac588997ccf42207da54c77fa
Manoj M <manumadhu1425@gmail.com>
2023-03-02 23:21:31 +0530

    2.15 Assisted Practice: Arrays

---
+++ JavaFSDAssistedPractices/src/com/assisstedPractices/section2/Arrays215.java
@@ -0,0 +1,25 @@
+package com.assisstedPractices.section2;
+
+public class Arrays215 {
+	public static void main(String[] args) {
+		intArray();
+		System.out.println();
+		stringArray();
+	}
+	
+	public static void intArray() {
+		int[] intArr = {2,3,4,5};
+		
+		for(int i: intArr) {
+			System.out.print(i+",");
+		}
+	}
+	
+	public static void stringArray() {
+		String[] strArr = {"Max","Torry","Abhi","Tex"};
+		
+		for(String i: strArr) {
+			System.out.print(i+",");
+		}
+	}
+}
